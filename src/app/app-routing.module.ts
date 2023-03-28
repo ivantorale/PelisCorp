2d1d47d33939f5cdea0c8e555215ddc456cd0fc8
ivantorale <ivantorale@campus.monlau.com>
2023-03-28 16:15:38 +0200

    rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from './user/signup/signup.component';
 import { FilmsComponent } from './list/films/films.component';
+import { PrincipalComponent } from './principal/principal/principal.component';
 
 const routes: Routes = [
+  {path: 'principal', component: PrincipalComponent },
   {path: 'signup', component: SignupComponent},
   {path: 'films', component: FilmsComponent},
 ];
